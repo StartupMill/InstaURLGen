21c192cd2e8c6c4f4614d96db64de9266d1f36d8
Toby Allen <toby@tobyallen.com>
2014-05-23 14:07:13 -0700

    First Version: Doesn't work right, but getting there.

--- scriptURL.js
+++ scriptURL.js
@@ -1,2 +1,13 @@
-
+<script language="javascript" type="text/javascript">
 var InstaURL = 'http://instagram.com/p/';
+function randomString() {
+	var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz-_";
+	var string_length = 10;
+	var randomstring = '';
+	for (var i=0; i<string_length; i++) {
+		var rnum = Math.floor(Math.random() * chars.length);
+		randomstring += chars.substring(rnum,rnum+1);
+	}
+	document.randform.randomfield.value = InstaURL.randomstring;
+}
+</script>
